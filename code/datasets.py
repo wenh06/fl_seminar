4c4eb40e89721c325caf8568f2d2702d6900fbc6
WEN Hao <wenh06@gmail.com>
2021-08-13 02:05:31 +0800

    add code reading datasets

---
+++ code/datasets.py
@@ -0,0 +1,21 @@
+"""
+dataset readers, including datasets from LIBSVM library, Cifar, etc.
+"""
+from typing import Tuple
+
+import numpy as np
+from sklearn.datasets import load_svmlight_file
+
+
+__all__ = [
+    "libsvmread",
+]
+
+
+def libsvmread(fp:str, multilabel:bool=False, toarray:bool=True) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    """
+    features, labels = load_svmlight_file(fp, multilabel=multilabel, dtype=np.float32)
+    if toarray:
+        features = features.toarray()
+    return features, labels
