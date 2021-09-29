4358c581cff50b45078c92dcf63d9e80f15cc700
WEN Hao <wenh06@gmail.com>
2021-09-29 13:57:06 +0800

    update cifar reader

--- code/data_processing/cifar.py
+++ code/data_processing/cifar.py
@@ -1,7 +1,10 @@
 """
 dataset readers, including Cifar10, Cifar100
 """
-from typing import NoReturn, Optional, List, Callable, Tuple
+
+import os
+import json
+from typing import NoReturn, Optional, List, Callable, Tuple, Dict, Sequence
 
 import numpy as np
 import torch
@@ -10,10 +13,23 @@ import torch.utils.data as data
 import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR10, CIFAR100
 
+from ..misc import CACHED_DATA_DIR
+
 
 __all__ = [
     "CIFAR_truncated", "CIFAR10_truncated", "CIFAR100_truncated",
+    "load_cifar_data", "partition_cifar_data",
 ]
+
+CIFAR_DATA_DIRS = {
+    n_class:os.path.join(CACHED_DATA_DIR, f"CIFAR{n_class}") for n_class in [10, 100,]
+}
+CIFAR_NONIID_CACHE_DIRS = {
+    n_class:os.path.join(CACHED_DATA_DIR, "non-iid-distribution", f"CIFAR{n_class}") for n_class in [10, 100,]
+}
+for n_class in [10, 100,]:
+    os.makedirs(CIFAR_DATA_DIRS[n_class], exist_ok=True)
+    os.makedirs(CIFAR_NONIID_CACHE_DIRS[n_class], exist_ok=True)
 
 
 class CIFAR_truncated(data.Dataset):
@@ -23,8 +39,8 @@ class CIFAR_truncated(data.Dataset):
     __name__ = "CIFAR10_truncated"
 
     def __init__(self,
-                 root:str,
                  n_class:int=10,
+                 root:Optional[str]=None,
                  dataidxs:Optional[List[int]]=None,
                  train:bool=True,
                  transform:Optional[Callable]=None,
@@ -32,8 +48,8 @@ class CIFAR_truncated(data.Dataset):
                  download:bool=False) -> NoReturn:
         """
         """
-        self.root = root
         self.n_class = n_class
+        self.root = root or CIFAR_DATA_DIRS[n_class]
         assert self.n_class in [10, 100]
         self.dataidxs = dataidxs
         self.train = train
@@ -46,8 +62,8 @@ class CIFAR_truncated(data.Dataset):
     def __build_truncated_dataset__(self) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         """
-        ds = {10: CIFAR10, 100: CIFAR100}[self.n_class]
-        cifar_dataobj = ds(self.root, self.train, self.transform, self.target_transform, self.download)
+        DS = {10: CIFAR10, 100: CIFAR100}[self.n_class]
+        cifar_dataobj = DS(self.root, self.train, self.transform, self.target_transform, self.download)
 
         data = cifar_dataobj.data
         target = np.array(cifar_dataobj.targets)
@@ -102,7 +118,7 @@ class CIFAR10_truncated(CIFAR_truncated):
                  download:bool=False) -> NoReturn:
         """
         """
-        super().__init__(root, 10, dataidxs, train, transform, target_transform, download)
+        super().__init__(10, root, dataidxs, train, transform, target_transform, download)
 
 
 class CIFAR100_truncated(CIFAR_truncated):
@@ -171,7 +187,9 @@ def _data_transforms_cifar() -> Tuple[Callable, Callable]:
     return train_transform, test_transform
 
 
-def load_cifar_data(datadir:str, n_class:int) -> Tuple[torch.Tensor,...]:
+def load_cifar_data(n_class:int, datadir:Optional[str]=None, to_numpy:bool=False) -> Tuple[Union[np.ndarray,torch.Tensor],...]:
+    """
+    """
     train_transform, test_transform = _data_transforms_cifar()
 
     cifar_train_ds = CIFAR_truncated(datadir, n_class, train=True, download=True, transform=train_transform)
@@ -179,5 +197,81 @@ def load_cifar_data(datadir:str, n_class:int) -> Tuple[torch.Tensor,...]:
 
     X_train, y_train = cifar_train_ds.data, cifar_train_ds.target
     X_test, y_test = cifar_test_ds.data, cifar_test_ds.target
+    
+    if to_numpy:
+        (X_train.cpu().numpy(), y_train.cpu().numpy(), X_test.cpu().numpy(), y_test.cpu().numpy())
+    else:
+        return (X_train, y_train, X_test, y_test)
 
-    return (X_train, y_train, X_test, y_test)
+
+def record_net_data_stats(y_train:torch.Tensor, net_dataidx_map:Dict[int,List[int]]) -> Dict[int,int]:
+    """
+    """
+    net_cls_counts = {}
+    for net_i, dataidx in net_dataidx_map.items():
+        unq, unq_cnt = np.unique(y_train[dataidx].cpu().numpy(), return_counts=True)
+        tmp = {unq[i]: unq_cnt[i] for i in range(len(unq))}
+        net_cls_counts[net_i] = tmp
+    return net_cls_counts
+
+
+def partition_cifar_data(dataset:CIFAR_truncated,
+                         datadir:str,
+                         partition:str,
+                         n_net:int,
+                         alpha:float,
+                         to_numpy:bool=False,) -> tuple:
+    """
+    """
+    n_class = dataset.n_class
+    X_train, y_train, X_test, y_test = load_cifar_data(n_class, datadir, to_numpy)
+    n_train = X_train.shape[0]
+    # n_test = X_test.shape[0]
+
+    if partition == "homo":
+        total_num = n_train
+        idxs = np.random.permutation(total_num)
+        batch_idxs = np.array_split(idxs, n_net)
+        net_dataidx_map = {i: batch_idxs[i] for i in range(n_net)}
+    elif partition == "hetero":
+        min_size = 0
+        K = 100
+        N = y_train.shape[0]
+        net_dataidx_map = {}
+
+        while min_size < 10:
+            idx_batch = [[] for _ in range(n_net)]
+            # for each class in the dataset
+            for k in range(K):
+                if to_numpy:
+                    idx_k = np.where(y_train == k)[0]
+                else:
+                    idx_k = np.where(y_train.cpu().numpy() == k)[0]
+                np.random.shuffle(idx_k)
+                proportions = np.random.dirichlet(np.repeat(alpha, n_net))
+                ## Balance
+                proportions = np.array([p * (len(idx_j) < N / n_net) for p, idx_j in zip(proportions, idx_batch)])
+                proportions = proportions / proportions.sum()
+                proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
+                idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
+                min_size = min([len(idx_j) for idx_j in idx_batch])
+
+        for j in range(n_net):
+            np.random.shuffle(idx_batch[j])
+            net_dataidx_map[j] = idx_batch[j]
+
+    elif partition == "hetero-fix":
+        dataidx_map_file_path = os.path.join(CIFAR_NONIID_CACHE_DIRS[n_class], "net_dataidx_map.json")
+        with open(dataidx_map_file_path, "r") as f:
+            net_dataidx_map = json.load(dataidx_map_file_path)
+
+    if partition == "hetero-fix":
+        distribution_file_path = os.path.join(CIFAR_NONIID_CACHE_DIRS[n_class], "distribution.json")
+        with open(distribution_file_path, "r") as f:
+            traindata_cls_counts = json.load(distribution_file_path)
+    else:
+        distribution_file_path = os.path.join(CIFAR_NONIID_CACHE_DIRS[n_class], "distribution.json")
+        with open(distribution_file_path, "w") as f:
+            traindata_cls_counts = json.dump(traindata_cls_counts, distribution_file_path, ensure_ascii=False)
+
+    return X_train, y_train, X_test, y_test, net_dataidx_map, traindata_cls_counts
--- code/misc.py
+++ code/misc.py
@@ -7,3 +7,5 @@ import os, sys
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 BUILTIN_DATA_DIR = os.path.join(PROJECT_DIR, "data")
+
+CACHED_DATA_DIR = os.path.join(PROJECT_DIR, ".data_cache")
