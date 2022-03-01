6db0c072b6602d5f796a88e407ce67627e23fdc8
WEN Hao <wenh06@gmail.com>
2022-03-01 20:42:27 +0800

    update experiment code

--- code/algorithms/_experiments/nodes.py
+++ code/algorithms/_experiments/nodes.py
@@ -1,17 +1,25 @@
 """
 """
 
-from dataclasses import dataclass
+import random
+from abc import ABC, abstractmethod
+from itertools import repeat
 from copy import deepcopy
-from typing import Optional, NoReturn, Iterable, List
+from typing import Any, Optional, NoReturn, Iterable, List, Sequence, Tuple
 
 import torch
 import torch.nn as nn
 from torch import Tensor
 from torch.utils.data import DataLoader
 from torch.nn.parameter import Parameter
+from torch.optim import Optimizer
+try:
+    from tqdm.auto import tqdm
+except ImportError:
+    from tqdm import tqdm
 
-from ...data_processing.fed_dataset import FedDataset
+from data_processing.fed_dataset import FedDataset
+from .optimizer import get_optimizer
 
 
 __all__ = [
@@ -19,25 +27,63 @@ __all__ = [
 ]
 
 
-@dataclass
 class SeverConfig:
     """
     """
-    num_iters: int
-    num_clients: int
-    num_selected_clients: Optional[int] = None
+    def __init__(self,
+                 algorithm:str,
+                 num_iters:int,
+                 num_clients:int,
+                 clients_sample_ratio:float) -> NoReturn:
+        """
+        """
+        self.algorithm = algorithm
+        self.num_iters = num_iters
+        self.num_clients = num_clients
+        self.clients_sample_ratio = clients_sample_ratio
 
 
-@dataclass
 class ClientConfig:
     """
     """
-    batch_size: int
-    learning_rate: float
-    num_epochs: int
+    def __init__(self,
+                 client_cls:str,
+                 algorithm:str,
+                 optimizer:str,
+                 batch_size:int,
+                 lr:float,
+                 num_epochs:int,) -> NoReturn:
+        """
+        """
+        self.client_cls = client_cls
+        self.algorithm = algorithm
+        self.optimizer = optimizer
+        self.batch_size = batch_size
+        self.lr = lr
+        self.num_epochs = num_epochs
 
 
-class Server:
+class Node(ABC):
+    """
+    """
+
+    @abstractmethod
+    def communicate(self, target:"Node") -> NoReturn:
+        """
+        communicate model parameters, gradients, etc. to `target` node
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self) -> NoReturn:
+        """
+        update model parameters, gradients, etc.
+        according to `self._reveived_messages`
+        """
+        raise NotImplementedError
+
+
+class Server(Node):
     """
     """
     def __init__(self,
@@ -52,50 +98,55 @@ class Server:
         self.criterion = criterion
         self.config = config
 
-        self._clients = [
-            Client(client_id, deepcopy(model), deepcopy(criterion), dataset, client_config) \
-                for client_id in range(config.num_clients)
+        self._clients = self._setup_clients(dataset, client_config)
+
+        self._received_messages = []
+
+    def _setup_clients(self, dataset:FedDataset, client_config:ClientConfig) -> List[Node]:
+        """
+        setup clients
+        """
+        client_cls = eval(client_config.client_cls)
+        return [
+            client_cls(client_id, device, deepcopy(self.model), deepcopy(self.criterion), dataset, client_config) \
+                for client_id, device in zip(range(self.config.num_clients), self._allocate_devices())
         ]
+
+    def _allocate_devices(self) -> List[torch.device]:
+        """
+        allocate devices for clients, can be used in `_setup_clients`
+        """
+        num_gpus = torch.cuda.device_count()
+        if num_gpus == 0:
+            return list(repeat(torch.device("cpu"), self.config.num_clients))
+        return [torch.device(f"cuda:{i%num_gpus}") for i in range(self.config.num_clients)]
+
+    def _sample_clients(self) -> List[int]:
+        """
+        sample clients for each iteration
+        """
+        k = int(self.config.num_clients * self.config.clients_sample_ratio)
+        return random.sample(range(self.config.num_clients), k)
 
     def train(self) -> NoReturn:
         """
         """
-        raise NotImplementedError
-
-    def add_grad(self, client:Client, ratio:float) -> NoReturn:
-        """
-        """
-        for param, client_grad in zip(self.model.parameters(), client.get_grads()):
-            if param.grad is not None and client_grad is not None:
-                param.grad = param.grad + client_grad.clone() * ratio
-
-    def aggregate_grads(self) -> NoReturn:
-        """
-        """
-        raise NotImplementedError
-
-    def send_parameters(self):
-        """
-        """
-        for c in self.clients:
-            c.set_parameters(self.model)
-
-    def add_parameters(self, user, ratio):
-        """
-        """
-        model = self.model.parameters()
-        for server_param, user_param in zip(self.model.parameters(), user.get_parameters()):
-            server_param.data = server_param.data + user_param.data.clone() * ratio
-
-    def aggregate_parameters(self):
-        raise NotImplementedError
+        with tqdm(range(self.config.num_iters), total=self.config.num_iters) as pbar:
+            for i in pbar:
+                for client_id in self._sample_clients():
+                    client = self._clients[client_id]
+                    self.communicate(client)
+                    client.update()
+                    client.communicate(self)
+                self.update()
 
 
-class Client:
+class Client(Node):
     """
     """
     def __init__(self,
                  client_id:int,
+                 device:torch.device,
                  model:nn.Module,
                  criterion:nn.Module,
                  dataset:FedDataset,
@@ -103,20 +154,39 @@ class Client:
         """
         """
         self.client_id = client_id
+        self.device = device
         self.model = model
         self.criterion = criterion
         self.dataset = dataset
         self.config = config
 
+        self._optimizer = get_optimizer(
+            optimizer_name=config.optimizer, params=self.model.parameters(), config=config
+        )
         self.train_loader, self.val_loader = \
             self.dataset.get_data_loaders(self.config.batch_size, self.config.batch_size, self.client_id)
+
+        self._global_model = None
+        self._received_messages = {}
 
     def train(self) -> NoReturn:
         """
         """
-        raise NotImplementedError
+        self.model.train()
+        epoch_losses = []
+        for epoch in range(self.config.num_epochs):
+            batch_losses = []
+            for batch_idx, (data, target) in enumerate(self.train_loader):
+                data, target = data.to(self.device), target.to(self.device)
+                self._optimizer.zero_grad()
+                output = self.model(data)
+                loss = self.criterion(output, target)
+                loss.backward()
+                self._optimizer.step()
+                batch_losses.append(loss.item())
+            epoch_losses.append(sum(batch_losses) / len(batch_losses))
 
-
+    @abstractmethod
     def evaluate(self) -> NoReturn:
         """
         """
@@ -125,7 +195,7 @@ class Client:
     def set_parameters(self, model:nn.Module) -> NoReturn:
         """
         """
-        raise NotImplementedError
+        self.update_parameters(new_params=model.parameters())
 
     def update_parameters(self, new_params:Iterable[Parameter]) -> NoReturn:
         """
--- code/algorithms/_experiments/optimizer.py
+++ code/algorithms/_experiments/optimizer.py
@@ -1,2 +1,36 @@
 """
 """
+
+from abc import ABC, abstractmethod
+from typing import Iterable, Any, Union, NoReturn
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.nn.parameter import Parameter
+from torch.optim import Optimizer
+
+from ..optimizers import get_optimizer as get_optimizer_
+
+
+__all__ = [
+    "get_optimizer",
+]
+
+
+def get_optimizer(optimizer_name:str, params:Iterable[Union[dict,Parameter]], config:Any) -> Optimizer:
+    """
+    """
+    try:
+        return get_optimizer_(optimizer_name, params, config)
+    except:
+        raise NotImplementedError
+
+
+class pFedOpt(Optimizer):
+    """
+    """
+    def __init__(self, params:Iterable[Union[dict,Parameter]], lr:float,) -> NoReturn:
+        """
+        """
+        raise NotImplementedError
