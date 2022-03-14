4fa6bf3ae9e383a4a0c930e455f9122bc71c5c8e
WEN Hao <wenh06@gmail.com>
2022-03-14 17:16:58 +0800

    allow get_optimizer get optimizer from Optimizer class

--- code/algorithms/optimizers/__init__.py
+++ code/algorithms/optimizers/__init__.py
@@ -1,13 +1,16 @@
 """
 """
 
+import inspect
 from typing import Iterable, Union, Any
 
 from torch.nn.parameter import Parameter
 from torch.optim import Optimizer
 import torch.optim as opt
 import torch_optimizer as topt
+from easydict import EasyDict as ED
 
+from misc import isclass
 from .base import ProxSGD
 from .fedpd import FedPD_SGD, FedPD_VR, PSGD, PSVRG
 from .pfedme import pFedMeOptimizer as pFedMe
@@ -22,7 +25,7 @@ __all__ = [
 ]
 
 
-def get_optimizer(optimizer_name:str, params:Iterable[Union[dict,Parameter]], config:Any) -> Optimizer:
+def get_optimizer(optimizer_name:Union[str,type], params:Iterable[Union[dict,Parameter]], config:Any) -> Optimizer:
     """ get optimizer by name
 
     Usage examples
@@ -35,18 +38,24 @@ def get_optimizer(optimizer_name:str, params:Iterable[Union[dict,Parameter]], co
     optimizer = get_optimizer("yogi", model.parameters(), {"lr": 1e-2})  # from pytorch_optimizer
     optimizer = get_optimizer("FedPD_SGD", model.parameters(), {"lr": 1e-2})  # federated
     """
+    if isclass(optimizer_name) and issubclass(optimizer_name, Optimizer):
+        print(f"{optimizer_name.__name__} is an Optimizer class, used directly")
+        return optimizer_name(params, **_get_args(optimizer_name, config))
     try:
-        optimizer = eval(f"opt.{optimizer_name}(params, **config)")
+        _config = _get_args(eval(f"opt.{optimizer_name}"), config)
+        optimizer = eval(f"opt.{optimizer_name}(params, **_config)")
         print(f"PyTorch built-in optimizer {optimizer_name} is used.")
         return optimizer
     except:
         try:
-            optimizer = topt.get(optimizer_name)(params, **config)
+            optimizer = topt.get(optimizer_name)(params, **_get_args(topt.get(optimizer_name), config))
             print(f"Optimizer {optimizer_name} from torch_optimizer is used.")
             return optimizer
         except:
             pass
 
+    if isinstance(config, dict):
+        config = ED(config)
     if optimizer_name == "FedPD_SGD":
         return FedPD_SGD(params, mu=config.mu, lamda=config.lamda, lr=config.lr)
     elif optimizer_name == "FedPD_VR":
@@ -63,3 +72,19 @@ def get_optimizer(optimizer_name:str, params:Iterable[Union[dict,Parameter]], co
         return FedDR(params, lr=config.lr, eta=config.eta)
     else:
         raise ValueError(f"Invalid optimizer name: {optimizer_name}")
+
+
+def _get_args(cls:type, config:Any) -> ED:
+    """
+    used to filter out the items in config that are not arguments of the class
+    """
+    if isinstance(config, dict):
+        config = ED(config)
+    args = [k for k in inspect.getfullargspec(cls.__init__).args if k not in ["self", "params",]]
+    kwargs = ED()
+    for k in args:
+        try:
+            kwargs[k] = eval(f"config.{k}")
+        except:
+            pass
+    return kwargs
