7dc05d117a6cc99e89b096d77f1d5e0048ae8514
WEN Hao <wenh06@gmail.com>
2022-05-07 11:13:55 +0800

    update formatting configs

--- formatting.sh
+++ formatting.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-black . --extend-exclude .ipynb -v
-flake8 . --count --ignore="E501 W503 E203 F841" --show-source --statistics --exclude=./.*,benchmarks,build,dist
+black . --extend-exclude .ipynb -v --exclude="/build|dist|benchmarks|boyd\-admm/"
+flake8 . --count --ignore="E501 W503 E203 F841" --show-source --statistics --exclude=./.*,benchmarks,build,dist,boyd-admm
